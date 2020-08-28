5b794e06831b0117afb626770303494096999d0f
Strom <stromkuo@gmail.com>
2020-08-28 16:21:23 +0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SeriesCapture
+{
+    class Program
+    {
+        const string testUrl = "http://www.zhuixinfan.com/viewtvplay-1193.html";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new Zhuixinfan().GetEpisodeUrls(testUrl);
+        }
+    }
+}
---
+++ Zhuixinfan.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesCapture
+{
+    class Zhuixinfan
+    {
+        public async Task<List<string>> GetEpisodeUrls(string seriesUrl)
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(seriesUrl);
+
+            var sss = htmlDoc.GetElementbyId("ajax_tbody").SelectNodes("//.td2");
+            foreach (var item in sss)
+            {
+            }
+            Console.WriteLine("a");
+            return null;
+        }
+    }
+}
