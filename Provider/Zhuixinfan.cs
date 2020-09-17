52ffd09ba54f1112375467fd0f6a3c28004ce5a4
Strom <stromkuo@gmail.com>
2020-09-17 17:47:25 +0800

    Added handling of html exception

--- Provider/Zhuixinfan.cs
+++ Provider/Zhuixinfan.cs
@@ -11,28 +11,49 @@ namespace SeriesCapture
         {
             var ret = new List<string>();
 
-            var urls = GetEpisodeUrls(seriesUrl);
-            foreach (var episodeUri in urls)
+            try
             {
-                ret.Add(await this.GetMagnetLink(episodeUri));
+                var urls = await GetEpisodeUrls(seriesUrl);
+                foreach (var episodeUri in urls)
+                {
+                    ret.Add(await this.GetMagnetLink(episodeUri));
+                }
+            }
+            catch (HtmlWebException e)
+            {
+                Console.WriteLine($"{e.Message}");
             }
 
             return ret;
         }
 
-        public List<Uri> GetEpisodeUrls(string seriesUrl)
+        public async Task<List<Uri>> GetEpisodeUrls(string seriesUrl)
         {
             var ret = new List<Uri>();
 
             HtmlWeb web = new HtmlWeb();
-            var htmlDoc = web.Load(seriesUrl);
-            var url = new Uri(seriesUrl);
+            var htmlDoc = await web.LoadFromWebAsync(seriesUrl);
+
+            if (htmlDoc == null)
+            {
+                throw new HtmlWebException($"Null html document loaded, url: {seriesUrl}");
+            }
+            else if (htmlDoc.DocumentNode == null)
+            {
+                throw new HtmlWebException($"Null node in html document, url: {seriesUrl}");
+            }
 
             var sss = htmlDoc.DocumentNode.SelectNodes("//*[@id='ajax_tbody']/tr/td[2]/a");
+
+            if (sss == null)
+            {
+                throw new HtmlWebException($"Episode urls node list is null, url: {seriesUrl}");
+            }
+
             foreach (var item in sss)
             {
                 var relative = item.GetAttributeValue("href", string.Empty);
-                ret.Add(new Uri(url, System.Web.HttpUtility.HtmlDecode(relative)));
+                ret.Add(new Uri(new Uri(seriesUrl), System.Web.HttpUtility.HtmlDecode(relative)));
             }
 
             return ret;
@@ -43,7 +64,22 @@ namespace SeriesCapture
             HtmlWeb web = new HtmlWeb();
             var htmlDoc = await web.LoadFromWebAsync(uri.AbsoluteUri);
 
+            if (htmlDoc == null)
+            {
+                throw new HtmlWebException($"Null html document loaded, url: {uri.AbsoluteUri}");
+            }
+            else if (htmlDoc.DocumentNode == null)
+            {
+                throw new HtmlWebException($"Null node in html document, url: {uri.AbsoluteUri}");
+            }
+
             var magnetLinkNode = htmlDoc.DocumentNode.SelectSingleNode("//*[@id='torrent_url']");
+
+            if (magnetLinkNode == null)
+            {
+                throw new HtmlWebException($"magnetLinkNode is null, url: {uri.AbsoluteUri}");
+            }
+
             return magnetLinkNode.InnerText;
         }
     }
